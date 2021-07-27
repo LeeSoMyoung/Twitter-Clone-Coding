5f2d8659ffac23fd27f1e8bc30fd068d3e9e351b
LeeSoMyoung <so4644009@gmail.com>
2021-07-27 23:48:42 +0900

    create an account
    이메일과 비밀번호를 통한 계정 생성까지 완료

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useEffect, useState } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'firebaseInstance';
 
 function App() {
-  return <AppRouter />
+
+  const [init, setInit] = useState(false);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        // 로그인 됐을 때 메인 화면을 띄우기 위해
+        setIsLoggedIn(true);
+      }
+      else {
+        // 로그아웃 시 로그인 화면을 띄우기 위해
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <>
+    {init? <AppRouter isLoggedIn={isLoggedIn} />:"Initializing..."}
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,74 @@
-import React from 'react';
+import { authService } from 'firebaseInstance';
+import React, { useState } from 'react';
 
-const Auth =() => <span>Auth</span>;
+const Auth = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = (event) => {
+        const {
+            target: { name, value }
+        } = event;
+
+        if (name === "email") {
+            setEmail(value)
+        }
+        else if (name === "password") {
+            setPassword(value);
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault(); // SPA이므로 refresh되어 정보들이 사라지는 것을 방지
+
+        try {
+
+            if (newAccount) {
+                // 새 계정 만들기
+                const data = authService.createUserWithEmailAndPassword(email, password);
+            }
+            else {
+                // 로그인 폼 submit
+                const data = authService.signInWithEmailAndPassword(email, password);
+            }
+
+        } catch (error) {
+            console.log(error);
+            setError((prev)=>error.message);
+        }
+    }
+
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="email" placeholder="email" required
+                    value={email}
+                    onChange={onChange} />
+                <input
+                    name="password"
+                    type="password" placeholder="password" required
+                    value={password}
+                    onChange={onChange} />
+                <input type="submit" value={newAccount ? "회원가입" : "로그인"} />
+                <div>{error}</div>
+            </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "로그인" : "계정 생성하기"}
+            </span>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 
 export default Auth;
